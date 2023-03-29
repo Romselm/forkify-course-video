8825da1b5c06dedc837543df40c0c6321a7dcebc
Romselm <gostevroman39@gmail.com>
2023-03-29 16:32:15 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b8418e7c-408b-4f41-8a73-0267e9947fed';
+export const MODAL_CLOSE_SEC = 2.5;
